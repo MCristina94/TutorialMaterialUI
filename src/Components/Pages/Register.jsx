7bc9da9f530f80201757678a0efb59879eda99e8
Maureen Garcia <cmaureen1908@gmail.com>
2024-07-25 10:11:55 -0500

    creacion del register

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { Box, Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
 
 const Register = () => {
+
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const validarEmail = (email) =>{
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(validarEmail(email)){
+      setError({
+        error: false,
+        message: ""
+      });
+      console.log("mensaje correcto");
+    }else{
+      setError({
+        error: true,
+        message: "Formate de email incorrecto"
+      });
+    }
+  };
   return (
-    <div>Register</div>
+    <>
+      <h1>Register</h1>
+      <Box component='form' onSubmit={handleSubmit}>
+        <TextField
+          id='email'
+          label='Email'
+          type='email'
+          variant='outlined'
+          fullWidth
+          required
+          helperText={error.message}
+          error={error.error}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button type='submit' variant='outlined' sx={{mt: 2}}>Registrame</Button>
+      </Box>
+      
+    </>
   )
 }
 
